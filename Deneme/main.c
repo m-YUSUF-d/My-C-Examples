699dd85b023ec97d8df9fcb13d5b664505c85fe2
Mustafa Yusuf <mydasdemir229@gmail.com>
2023-12-28 15:08:02 +0300

    FolderCommands
    FolderCommands added.

--- Deneme/main.c
+++ Deneme/main.c
@@ -795,6 +795,40 @@ struct Person
 };
 
 
+void FolderCommands()
+{
+    //write
+    FILE *fp;
+    fp = fopen("dosya.txt" , "w");
+
+    if(fp != NULL)
+    {
+        fprintf(fp , "%s" , "Hello World !");
+        fclose(fp);
+    }
+
+
+    //read
+    char reading[100];
+    fp = fopen("dosya.txt" , "r");
+
+    if(fp == NULL)
+    {
+        printf("dosya.txt is not existed.");
+        exit(0);
+    }
+
+    fgets(reading, sizeof(reading), fp);
+    puts(reading);
+
+
+    fclose(fp);
+}
+
+
+
+
+
 
 //********************
 
@@ -1143,6 +1177,10 @@ int main()
     }
 
 
+    /**FolderCommands**/
+    {
 
+        FolderCommands();
 
+    }
 }
